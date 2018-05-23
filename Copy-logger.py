1a2f47c7c98af1e2d66a726fa9b6c7dce3fe7fc6
jeswivinay <ejsf114@gmail.com>
2018-05-23 16:35:48 +0530

    Add files via upload
    First commi -v0

---
+++ Copy-logger.py
@@ -0,0 +1,27 @@
+import os
+import pyperclip as ppp
+import time
+
+"""fname=raw_input("Enter the file name")"""
+savepath=os.getcwd()
+fname=time.strftime("%d-%m-%y")
+cn=os.path.join(savepath,fname+'.txt')
+oldcopy="sample"
+space=0
+
+while(1):
+        f2=open(cn,'a+')
+        content=ppp.paste()
+        content=content[0:50]
+        """if(len(content)<50):                 //trying to put spaces
+                sp=len(content)-50
+                while(sp):
+                        content=content+" "
+                        sp=sp-1"""
+        if(oldcopy==content):
+            continue
+        f2.write(content)
+        f2.write(time.strftime('%c'))
+        f2.write("\n")
+        oldcopy=content
+        f2.close()
